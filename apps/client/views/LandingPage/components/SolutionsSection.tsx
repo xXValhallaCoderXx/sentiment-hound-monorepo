5b233873f0c4b82213fcd457fdaf1e959988ea4c
Renate <renate.gouveia@gmail.com>
2023-08-19 01:59:48 +0800

    Clean up routes

--- apps/client/views/LandingPage/components/SolutionsSection.tsx
+++ apps/client/views/LandingPage/components/SolutionsSection.tsx
@@ -1,14 +1,22 @@
-import { Text, Box } from "@chakra-ui/react";
+import { Text, Box, Heading } from "@chakra-ui/react";
 
 const SolutionsSection = () => {
   return (
-    <Box>
-      <Text>Explore the Solutions</Text>
-      <Text>
-        Make data-driven choices with confidence and stay ahead of the curve.
-        Experience the power of our sentiment analysis product today and
-        revolutionize your decision-making process.
-      </Text>
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        mt={8}
+        sx={{ width: 800 }}
+      >
+        <Heading>Explore the Solutions</Heading>
+        <Text mt={2} align="center">
+          Make data-driven choices with confidence and stay ahead of the curve.
+          Experience the power of our sentiment analysis product today and
+          revolutionize your decision-making process.
+        </Text>
+      </Box>
     </Box>
   );
 };
