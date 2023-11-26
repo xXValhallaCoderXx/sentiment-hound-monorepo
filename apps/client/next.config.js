cbfcbc62cde0932f3ea3e4521c5b84f93515a628
xXValhallaCoderXx <renate.gouveia@gmail.com>
2023-11-26 18:56:07 +0800

    Centralized the .env file

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 console.log("ENVIRONENT", process.env.NODE_ENV);
-const path = require('path');
-const dotenv = require('dotenv');
 
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const nextConfig = {
   reactStrictMode: true,
 }
